23175b6e1c89bf469010ff841e272598f3ee31a7
Ronya <klmirg6@gmail.com>
2022-03-15 00:02:04 -0500

    finished the User model and added bcrypt for password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   firstName: {
@@ -27,6 +29,19 @@ const userSchema = new Schema({
     minLength: 4
   }
 })
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
 
 
 const User = mongoose.model('User', userSchema);
