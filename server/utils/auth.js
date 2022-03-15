50ec5b7b869e8d36c3f258909fb8b7e3d565dadb
Ronya <klmirg6@gmail.com>
2022-03-15 00:39:05 -0500

    set up the authMiddleware function

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,28 @@ const secret = process.env.SECRET
 const expiration = '2h';
 
 module.exports = {
+
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop.trim();
+    }
+
+    if (!token) {
+      return req
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  
   signToken: function ({ firstName, phoneNumber, _id }) {
     const payload = { firstName, phoneNumber, _id };
 
