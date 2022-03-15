e5903ffd8ff8b1a8cd932c7ffe34d373dffc3111
Ronya <klmirg6@gmail.com>
2022-03-15 00:33:11 -0500

    set up the auth.js server side to use a jwt as well as made a .env file to protect sensitive information

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const secret = process.env.SECRET
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ firstName, phoneNumber, _id }) {
+    const payload = { firstName, phoneNumber, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
