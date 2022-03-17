73c3bf8c6afc8ff8a79d12002a2a839fcf6219e4
glanctot <gregory.lanctot28@gmail.com>
2022-03-16 20:18:50 -0500

    push

--- server/models/User.js
+++ server/models/User.js
@@ -18,8 +18,8 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
-    // savedBooks: [bookSchema],
+    // set savedDogs to be an array of data that adheres to the bookSchema
+    // savedDogs: [dogSchema],
   },
   // set this to use virtual below
   {
