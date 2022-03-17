317ce65fc5b6e9f2670867f692bb3c26312cc36e
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-17 17:54:12 -0500

    worked on getting semantic dowloaded

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 // import { Form, Button, Modal } from 'react-bootstrap';
 
 // import Auth from '../utils/auth';
@@ -7,85 +8,39 @@ import React, { useState } from 'react';
 
 
 const SignupForm = ({ handleClick }) => {
-
-
-
-
-  // const [userFormData, setUserFormData] = useState({ name: '', phoneNumber: '', password: '' });
-  // // set state for form validation
-  // const [validated] = useState(false);
-  // // set state for alert
-  // const [showAlert, setShowAlert] = useState(false);
-
-  // const [addUser] = useMutation(ADD_USER);
-
-  // const handleInputChange = (event) => {
-  //   const { name, value } = event.target;
-  //   setUserFormData({ ...userFormData, [name]: value });
-  // };
-
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   // check if form has everything (as per react-bootstrap docs)
-  //   const form = event.currentTarget;
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
-
-  //   try {
-  //     const { data } = await addUser({
-  //       variables: {...userFormData},
-  //     });
-    
-  //     Auth.login(data.addUser.token)
-
-  //   } catch (err) {
-  //     console.error(err);
-  //     setShowAlert(true);
-  //   }
-
-  //   setUserFormData({
-  //     name: '',
-  //     phoneNumber: '',
-  //     password: '',
-  //   });
-  // };
-
   return (
     <>
-     <div className="modal-test" style={{backgroundColor: 'blue'}}>
-     <div>
-      <h2>Sign Up Here: </h2>
-      <form>
-        <input
-          placeholder='Your name here'
-          name='name'
-          type='text'
-          id='name'
-        />
-        
-        <input
-          placeholder='Your phone number'
-          name='phoneNumber'
-          type='phoneNumber'
-          id='phoneNumber'
-          // onChange={handleChange}
-        />
-        <input
-          placeholder='*********'
-          name='password'
-          type='password'
-          id='pwd'
-          // onChange={handleChange}
-        />
-      </form>
-    </div>
-       <button onClick={() => handleClick()}>Submit</button>
-     </div>
-      
-    {/* <Button onClick={handleShow}>Sign up</Button>
+      <div className="modal-test" style={{ backgroundColor: 'blue' }}>
+        <div>
+          <h2>Sign Up Here: </h2>
+          <form>
+            <input
+              placeholder='Your name here'
+              name='name'
+              type='text'
+              id='name'
+            />
+
+            <input
+              placeholder='Your phone number'
+              name='phoneNumber'
+              type='phoneNumber'
+              id='phoneNumber'
+            // onChange={handleChange}
+            />
+            <input
+              placeholder='*********'
+              name='password'
+              type='password'
+              id='pwd'
+            // onChange={handleChange}
+            />
+          </form>
+        </div>
+        <button onClick={() => handleClick()}>Submit</button>
+      </div>
+
+      {/* <Button onClick={handleShow}>Sign up</Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Sign up here!</Modal.Title>
@@ -99,7 +54,7 @@ const SignupForm = ({ handleClick }) => {
         </Modal.Footer>
       </Modal> */}
     </>
-      
+
   )
 }
 
