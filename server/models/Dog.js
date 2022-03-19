0bd1f755f404c8aeb05b4abeea9d1c99c1311049
glanctot <gregory.lanctot28@gmail.com>
2022-03-18 22:11:59 -0500

    working on saving dogs

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -1,7 +1,21 @@
 const { Schema } = require('mongoose');
 
 const dogSchema = new Schema({
-
+    image: {
+        type: String
+    },
+    breed: {
+        type: String
+    },
+    characteristics: {
+        type: String
+    },
+    life_span: {
+        type: String
+    },
+    weight: {
+        type: String
+    }
 })
 
 module.exports = dogSchema;
