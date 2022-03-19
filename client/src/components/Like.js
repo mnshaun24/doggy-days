3e9e6f3be3775f74a8f91147d7e5ad3b6011c876
Molly Montgomery <Molly@mollys-air-2.lan>
2022-03-18 20:40:52 -0500

    fixed dog image size and added bootstrap to buttons on dislike/like

--- client/src/components/Like.js
+++ client/src/components/Like.js
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
+import { Button } from "react-bootstrap";
 
 const Like = () => {
 
     const [showMeNext, setShowMeNext] = useState(false);
 
     return (
-        <button onClick={() => setShowMeNext(true)}>
+        <Button variant="outline-light" onClick={() => setShowMeNext(true)}>
             {/* insert graphic here */} this is like
-        </button>
+        </Button>
     )
 };
 
