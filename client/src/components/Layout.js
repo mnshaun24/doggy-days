6608319e720ab2512fb2c1d634611819a50716ba
Ronya <klmirg6@gmail.com>
2022-03-21 23:47:35 -0500

    working on the images

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,46 +1,48 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
-import {bubble as Menu} from "react-burger-menu";
-
+import { bubble as Menu } from "react-burger-menu";
 
 import Settings from "../pages/Settings";
 
-import Nav from 'react-bootstrap/Nav';
-
+import Nav from "react-bootstrap/Nav";
 
 const Layout = () => {
-    return (
-        <>
+  return (
+    <>
+      <nav>
+        <ul>
+          <Menu>
+            <div>
+              <Settings>Settings</Settings>
+            </div>
+          </Menu>
 
-        <nav>
-            <ul>
-                <Menu>
-                    <div>
-                    <Settings>Settings</Settings>
-                    </div>
-            </Menu>
-              
+          {/* <Link to="settings"><img className="settings" src={require('../assets/images/settings.png')} alt="settings" /></Link> */}
 
-           
-                 {/* <Link to="settings"><img className="settings" src={require('../assets/images/settings.png')} alt="settings" /></Link> */}
-                        
-                    
-                    <li>
-                    <Link to="home"><img className="headerlogo" src={require('../assets/images/logo.png')} alt="logo" /></Link>
-                    </li>
-                    <li>
-                    <Link to="saved"><img className="circle" src={require('../assets/images/circle.png')} alt="saved" /></Link>
-                    </li>
-                </ul>
-            </nav>
+          <li>
+            <Link to="home">
+              <img
+                className="headerlogo"
+                src={require("../assets/images/logo.png")}
+                alt="logo"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link to="saved">
+              <img
+                className="circle"
+                src={require("../assets/images/circle.png")}
+                alt="saved"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-
-            <Outlet />
-        </>
-    )
-}
+      <Outlet />
+    </>
+  );
+};
 
 export default Layout;
-
-
-
