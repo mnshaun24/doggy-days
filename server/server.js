e14e58fc9816e64888bba7bc09ec78537a791925
Shaun Sticka <shaun.sticka@outlook.com>
2022-03-23 16:49:36 -0500

    changed if statement in server

--- server/server.js
+++ server/server.js
@@ -1,46 +1,43 @@
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        context: authMiddleware
-    })
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
 
-    await server.start();
+  await server.start();
 
-    server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
-    
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../client/build/index.html'));
-        });
-    }
-
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-}
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
 startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on ${PORT}!`)
-    })
-})
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on ${PORT}!`);
+  });
+});
