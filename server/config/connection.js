f8ddba51c7afd999a1370ca49453d6292b4c690f
Shaun Sticka <shaun.sticka@outlook.com>
2022-03-23 14:31:34 -0500

    verified mongodb url string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dinder', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dinder-dog', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
